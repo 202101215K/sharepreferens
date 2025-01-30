8c03c8644aa8ac687a70cebf37e95577c3dc8d88
202101215K <202101215k@utea.edu.pe>
2025-01-29 21:35:17 -0500

    HomeCompleto

--- lib/pages/Home_page.dart
+++ lib/pages/Home_page.dart
@@ -7,8 +7,9 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("SharedPreferences App"),
-        centerTitle: true,
+        title: const Text("Home"),
+        backgroundColor: const Color(0xFF3F51B5),
+        foregroundColor: Colors.white,
       ),
       drawer: Drawer(
         child: Column(
@@ -30,47 +31,133 @@ class HomePage extends StatelessWidget {
                     CircleAvatar(
                       radius: 30.0,
                       backgroundColor: Colors.amber,
-                      backgroundImage: NetworkImage("https://images.pexels.com/photos/2413089/pexels-photo-2413089.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"),
+                      backgroundImage: NetworkImage(
+                          "https://images.pexels.com/photos/2413089/pexels-photo-2413089.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"),
                     ),
-                    Text("Leonel messi", style: TextStyle(color: Colors.white,
-                    fontSize: 16.0,fontWeight: FontWeight.bold),
+                    Text(
+                      "Leonel messi",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.bold),
                     ),
-                    Text("Administrador", style: TextStyle(color: Colors.white,
-                    fontSize: 16.0,fontWeight: FontWeight.w400),
+                    Text(
+                      "Administrador",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.w400),
                     ),
-                    
                   ],
                 ),
-                
               ),
-              
             ),
-          ListTile(
-          leading: Icon(Icons.person),
-          title: Text("Myprofile"),
-          onTap: (){},
-          ),
-          ListTile(
-          leading: Icon(Icons.work),
-          title: Text("Portfolio"),
-          onTap: (){},
-          ),
-          ListTile(
-          leading: Icon(Icons.lock),
-          title: Text("Change password"),
-          onTap: (){},
-          ),
-          Divider(height: 30.0,),
-          ListTile(
-          leading: Icon(Icons.logout_outlined),
-          title: Text("Logout"),
-          onTap: (){},
-          ),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text("Myprofile"),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.work),
+              title: Text("Portfolio"),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.lock),
+              title: Text("Change password"),
+              onTap: () {},
+            ),
+            Divider(
+              height: 30.0,
+            ),
+            ListTile(
+              leading: Icon(Icons.logout_outlined),
+              title: Text("Logout"),
+              onTap: () {},
+            ),
           ],
         ),
       ),
-      body: Column(
-        children: [Text("Hola")],
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              'Configuración general',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 20),
+            const TextField(
+              decoration: InputDecoration(
+                labelText: 'Elvis'
+              ),
+            ),
+            const SizedBox(height: 16),
+            const TextField(
+              decoration: InputDecoration(
+                labelText: 'Lopez'
+              ),
+            ),
+            const SizedBox(height: 16),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text('Dark Mode'),
+                Switch(
+                  value: false,
+                  activeColor: Colors.black,
+                  onChanged: (value) {
+
+                  },
+                ),
+              ],
+            ),
+            const SizedBox(height: 16),
+            const Text('Gender'),
+            RadioListTile(
+              title: const Text('Male'),
+              value: 'male',
+              groupValue: 'male',
+              activeColor: Colors.black,
+              onChanged: (value) {},
+            ),
+            RadioListTile(
+              title: const Text('Female'),
+              value: 'female',
+              groupValue: 'male',
+              activeColor: Colors.black,
+              onChanged: (value) {
+                
+              },
+            ),
+            const SizedBox(height: 20),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton.icon(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.black,
+                  disabledBackgroundColor: Colors.black,
+                  padding: const EdgeInsets.symmetric(vertical: 16),
+                ),
+                onPressed: (){
+
+                },
+                icon: const Icon(Icons.save, color: Colors.white),
+                label: const Text(
+                  'Save data',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
